7860fc456f0e042016bf8422954c081e9271c20b
Kyle Goyette <kdgoyette@gmail.com>
2022-01-25 21:47:12 -0800

    fox

--- model_registry_example/model_trainer.py
+++ model_registry_example/model_trainer.py
@@ -6,6 +6,7 @@
 import wandb
 import util
 import argparse
+import os
 
 project             = "model_registry_ex"
 model_use_case_id   = "mnist"
@@ -14,10 +15,12 @@ job_type            = "model_trainer"
 # First, we launch a run which registers this workload with WandB.
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size',         type=int,   default=128)
+parser.add_argument('train')
 parser.add_argument('--epochs',             type=int,   default=5)
 parser.add_argument('--optimizer',          type=str,   default="adam")
 parser.add_argument('--validation_split',   type=float, default=0.1)
-run = wandb.init(project=project, job_type=job_type, config=parser.parse_args())
+args, _ = parser.parse_known_args()
+run = wandb.init(project=project, job_type=job_type, config=args)
 
 # Next we download the latest training data available for this use case from WandB. 
 # Again, the domain specific logic is abstracted away in a helper function.
@@ -25,7 +28,10 @@ x_train, y_train = util.download_training_dataset_from_wb(model_use_case_id)
 
 # Then we train a model using this data. For simplicity, we use a sequential model.
 model = util.build_and_train_model(x_train, y_train, config=run.config)
+os.makedirs("/opt/ml/model", exist_ok=True)
+model.save(f"/opt/ml/model/{model_use_case_id}_model.h5")
 
 # Finally, we publish the model to WandB. This will create a new artifact version
 # that serves as a "candidate" model for this use case.
 util.publish_model_candidate_to_wb(model, model_use_case_id)
+
